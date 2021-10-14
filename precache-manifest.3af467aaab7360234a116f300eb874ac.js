793436a2ae1af804774117c63ccf6d2fd3b74b21
William-La <wla@g.hmc.edu>
2021-10-14 14:46:35 -0700

    Updates

---
+++ precache-manifest.3af467aaab7360234a116f300eb874ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb56d611f457fe98af885d75111aa134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5654748285e3f9dfcd71",
+    "url": "/static/css/main.082c46f1.chunk.css"
+  },
+  {
+    "revision": "60dbf4626c237b9087dc",
+    "url": "/static/js/2.94d7698e.chunk.js"
+  },
+  {
+    "revision": "eb314788fe3fb32ce23a1d6fa9676554",
+    "url": "/static/js/2.94d7698e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f282d1a2f4c848bdc56f",
+    "url": "/static/js/3.23c7e9a5.chunk.js"
+  },
+  {
+    "revision": "5654748285e3f9dfcd71",
+    "url": "/static/js/main.9fe30eda.chunk.js"
+  },
+  {
+    "revision": "0d79879985ade3cdca9f",
+    "url": "/static/js/runtime-main.a8fc9d74.js"
+  }
+]);

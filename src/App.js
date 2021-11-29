90f77a1ead8e787b51a71dcf0ef27646d257a208
William-La <wla@g.hmc.edu>
2021-11-29 14:39:28 -0800

    end of monday class

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 import { useState } from 'react';
 import firebase from "firebase/compat";
 import {useCollection} from "react-firebase-hooks/firestore";
-import { getAnalytics } from "firebase/analytics";
 import {
     useAuthState,
     useCreateUserWithEmailAndPassword,
@@ -24,11 +23,89 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+const auth = firebase.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+
+function App(props) {
+    const [user, loading, error] = useAuthState(auth);
+
+    function verifyEmail() {
+        auth.currentUser.sendEmailVerification();
+    }
+
+    if (loading) {
+        return <p>Checking...</p>;
+    } else if (user) {
+        return <div>
+            {user.displayName || user.email}
+            <SignedInApp {...props} user={user}/>
+            <button type="button" onClick={() => auth.signOut()}>Logout</button>
+            {!user.emailVerified && <button type="button" onClick={verifyEmail}>Verify email</button>}
+        </div>
+    } else {
+        return <>
+            {error && <p>Error App: {error.message}</p>}
+                <SignIn key="Sign In"/>
+                <SignUp key="Sign Up"/>
+        </>
+    }
+}
+
+const FAKE_EMAIL = 'foo@bar.com';
+const FAKE_PASSWORD = 'xyzzyxx';
+
+function SignIn() {
+    const [
+        signInWithEmailAndPassword,
+        userCredential, loading, error
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (userCredential) {
+        // Shouldn't happen because App should see that
+        // we are signed in.
+        return <div>Unexpectedly signed in already</div>
+    } else if (loading) {
+        return <p>Logging in…</p>
+    }
+    return <div>
+        {error && <p>"Error logging in: " {error.message}</p>}
+        <button onClick={() =>
+            signInWithEmailAndPassword(FAKE_EMAIL, FAKE_PASSWORD)}>Login with test user Email/PW
+        </button>
+        <button onClick={() =>
+            auth.signInWithPopup(googleProvider)}>Login with Google
+        </button>
+    </div>
+}
+
+function SignUp() {
+    const [
+        createUserWithEmailAndPassword,
+        userCredential, loading, error
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    if (userCredential) {
+        // Shouldn't happen because App should see that
+        // we are signed in.
+        return <div>Unexpectedly signed in already</div>
+    } else if (loading) {
+        return <p>Signing up…</p>
+    }
+    return <div>
+        {error && <p>"Error signing up: " {error.message}</p>}
+        <button onClick={() =>
+            createUserWithEmailAndPassword(FAKE_EMAIL, FAKE_PASSWORD)}>
+            Create test user
+        </button>
+
+    </div>
+}
 
 
 const tasks_collection = "william-la-tasks";
 const tabs_collection = "william-la-tab";
-function App() {
+function SignedInApp(props) {
     const [tab, setTab] = useState("0");
     const [view, setView] = useState('all');
     const [sort, setSort] = useState('date');
@@ -65,10 +142,13 @@ function App() {
         const newId = generateUniqueID();
         db.collection(tabs_collection).doc(newId).set({
             tabId: newId,
-            title: value
+            title: value,
+            owner: props.user.uid
         })
     }
-
+    if (error) {
+        console.log(error.message);
+    }
     return <div>
         {tabs && <Header view={handleView}
                 sort={handleSort}
